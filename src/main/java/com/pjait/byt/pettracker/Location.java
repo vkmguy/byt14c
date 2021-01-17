68ebe646e73819a1da43bcbbf7c23693c67bbeb9
Volodymyr <s19159@pjwstk.edu.pl>
2021-01-17 21:10:37 +0200

    add Collar, CollarTest, Location, LocationTest, refactor TestToFIX

--- src/main/java/com/pjait/byt/pettracker/Location.java
+++ src/main/java/com/pjait/byt/pettracker/Location.java
@@ -6,6 +6,19 @@ import lombok.Setter;
 @Getter
 @Setter
 public class Location {
-    private int lat;
-    private int lon;
+    private final double lat;
+    private final double lon;
+
+    public Location(double lat, double lon) {
+        this.lat = lat;
+        this.lon = lon;
+    }
+
+    public double getLat() {
+        return lat;
+    }
+
+    public double getLon() {
+        return lon;
+    }
 }
